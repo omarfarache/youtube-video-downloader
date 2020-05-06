b367770bf2e793d8805b35c25dcf1b5fff568fdd
Omar <omarfarache91@gmail.com>
2020-05-06 22:59:04 +0300

    v1.1

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
 
 var config = require('./config');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
